3415988c17cc114fb850a11a03dffbe057ec3b93
Arthur Bernardo <arthurutschgg@gmail.com>
2021-06-13 22:14:08 -0300

    funcionalidade comentario

--- js/json.js
+++ js/json.js
@@ -1,31 +1,51 @@
 URL = 'http://localhost:3000/feedbacks'
 
-const feedback = document.getElementById('show_feedback');
+const name = document.getElementById('name')
+const description = document.getElementById('description')
+const feedback = document.getElementById('feedback')
+const button = document.querySelector("#button").addEventListener("click",() => {
+    const create_feedback = JSON.stringify({
+        name:name.value, description:description.value, feedback:feedback.value
+    })
+    
+    console.log({create_feedback})
+    fetch(URL, {method: "POST",headers:{
+        "Content-Type":"application/json"
+    }, body:create_feedback
+}).then(res => res.json).then(() => location.reload())
+})
+
+
+const feed = document.getElementById('show_feedback');
+
+function deletar(id) {
+    fetch(`${URL}/${id}`,{method:"DELETE"}).then(res => res.json()).then(() => {
+        location.reload()
+    })
+}
 
 fetch(URL)
-    .then(res => res.json())
-    .then(feedbacks => {
-        let lista_feedback = "";
-        for (let i = 0; i < feedbacks.length; i++) {
-            lista_feedback += `
+.then(res => res.json())
+.then(feedbacks => {
+    let lista_feedback = "";
+    for (let i = 0; i < feedbacks.length; i++) {
+        lista_feedback += `
+            <div class="comentario">
+            <span>${feedbacks[i].name}</span>
+            <div class ="content_feedback">
+            <span>${feedbacks[i].description}</span>
+            <span> Avaliação: ${feedbacks[i].feedback} ⭐</span>
+            </div>
             <div>
-                <span>${feedbacks[i].id}</span>
-                <span>${feedbacks[i].nome}</span>
-                <span>${feedbacks[i].comentario}</span>
-                <div>
-                    <a onclick="getProduto(${feedbacks[i].id});" 
-                    data-toggle="modal" data-target="#produto-modal">
-                    <i></i>  Editar
-                    </a>
 
-                    <a onclick="$('#id-prod').text(${feedbacks[i].id});
-                    data-toggle="modal" data-target="#modal-delete">
-                    <i></i> Remover
-                    </a>
-                </div>
+            <a onclick="deletar(${feedbacks[i].id})" id="deletar">
+            <i></i> Remover
+            </a>
+            </div>
             </div>
             `;
-            console.log(lista_feedback)
-            feedback.innerHTML = lista_feedback
+
+            feed.innerHTML = lista_feedback
         }
     });
+   
