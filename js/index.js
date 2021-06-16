243a1db63062dbca3dd01c3877edce451ef44ae0
Arthur bernardo <arthurutschgg@gmail.com>
2021-06-16 19:12:58 -0300

    site pronto

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 const list = document.querySelectorAll(".container-perguntas section");
-const menu = document.querySelector(".menu_open");
-const view_menu = document.querySelector(".view_menu");
 list.forEach((element) => {
   element.addEventListener("click", () => {
     let pergunta = element.children[0];
@@ -20,25 +18,4 @@ list.forEach((element) => {
   });
 });
 
-window.addEventListener("resize", (e) => {
-  const view = e.target.visualViewport.width;
-  if (view <= 1035) {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
-  console.log(view);
-});
 
-menu.addEventListener("click", () => {
-  let isActive = "";
-  if (menu.style.display === "block") {
-    isActive =
-      menu.className === "menu_open"
-        ? (menu.className = "active")
-        : (menu.className = "menu_open");
-  }
-  isActive === "active"
-    ? (view_menu.style.display = "block")
-    : (view_menu.style.display = "none");
-});
